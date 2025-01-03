6814416168747472db7f7d85917a7a7757b14be3
Marek Sierociński <marek.sierocinski@cdq.com>
2025-01-03 09:14:11 +0100

    other: Fix docs

--- examples/data-sources/githubcrypt_encrypted_environment_secret/data.tf
+++ examples/data-sources/githubcrypt_encrypted_environment_secret/data.tf
@@ -1,4 +1,4 @@
-data "githubcrypt_encrypted_environment_secret" "reference-data-fetch-and-transform-job_prod_argocd_user_secret" {
+data "githubcrypt_encrypted_environment_secret" "my_user_secret" {
   public_key_base64 = data.githubcrypt_environment_public_key.my_environment_public_key.public_key
   secret = var.my_secret
 }
